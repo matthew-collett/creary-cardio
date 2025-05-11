0422e3e4a4b2d25752e03d280c8a431851b18410
matthew-collett <matthew.collett12@gmail.com>
2025-05-11 19:48:14 -0300

    fix: Fix scroll issues on mobile

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -6,7 +6,7 @@ import { Card } from '@/components/core'
 const AppLayout = () => (
   <div className="flex h-dvh w-full bg-primary">
     <Sidebar />
-    <main className="flex-1 overflow-y-auto py-6 pr-6 max-md:pl-6">
+    <main className="flex-1 overflow-y-auto py-6 pr-6 max-md:pl-6 bg-primary">
       <Card className="min-h-full rounded-bl-none border-none">
         <Header />
         <Outlet />

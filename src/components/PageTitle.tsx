9cc9ad72d635a79fb82fdd528c8d4ad25f839a3c
matthew-collett <matthew.collett12@gmail.com>
2025-05-11 19:17:35 -0300

    fix: Fix scroll issues on mobile

--- src/components/PageTitle.tsx
+++ src/components/PageTitle.tsx
@@ -12,7 +12,7 @@ export const PageTitle = ({
   iconComponent?: ReactNode
   title: string
 }) => (
-  <div className="flex justify-between items-center py-4 gap-2 max-sm:flex-col">
+  <div className="flex justify-between items-center py-4 gap-2 max-sm:flex-col h-page-title max-sm:h-page-title-sm">
     <div className="flex items-center gap-2 max-sm:text-3xl text-4xl font-light">
       {Icon ? <Icon size="1.25em" /> : iconComponent}
       <span>{title}</span>

07cf2a99a480fc91b09fb4d87438d889624ba18d
matthew-collett <matthew.collett12@gmail.com>
2025-05-11 18:24:34 -0300

    fix: Loader card height

--- src/components/Calendar/LoaderCard.tsx
+++ src/components/Calendar/LoaderCard.tsx
@@ -2,7 +2,7 @@ import { Loader } from '@/components'
 import { Card } from '@/components/core'
 
 export const LoaderCard = () => (
-  <Card className="bg-white flex justify-center items-center h-[var(--sx-calendar-calculated-height)]">
+  <Card className="bg-white flex justify-center items-center h-[var(--sx-calendar-calculated-height)] max-sm:h-[var(--sx-calendar-calculated-height-sm)]">
     <Loader />
   </Card>
 )
